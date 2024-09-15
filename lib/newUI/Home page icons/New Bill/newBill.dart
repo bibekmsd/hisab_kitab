b88d6daa7a519d4d8e748294d710611848c22e12
Anoobee <aryalanup13@gmail.com>
2024-09-15 23:12:40 +0545

    edit price works

--- lib/newUI/Home page icons/New Bill/newBill.dart
+++ lib/newUI/Home page icons/New Bill/newBill.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_print, use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:hisab_kitab/newUI/Home%20page%20icons/Add%20Products/nabhetekoProductAdd.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'check_out.dart';
+import 'package:hisab_kitab/newUI/Home%20page%20icons/New%20Bill/edit.dart'; // Import the edit page
 
 class Newbill extends StatefulWidget {
   const Newbill({super.key});
@@ -14,7 +12,7 @@ class Newbill extends StatefulWidget {
 }
 
 class _NewbillState extends State<Newbill> {
-  final List<String> _scannedValues = [];
+  final List<Map<String, dynamic>> _scannedProducts = [];
   final TextEditingController barcodeController = TextEditingController();
   bool isScanning = false;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -40,22 +38,19 @@ class _NewbillState extends State<Newbill> {
       if (querySnapshot.docs.isNotEmpty) {
         final productData = querySnapshot.docs.first.data();
         setState(() {
-          if (!_scannedValues.contains(productData['Barcode'])) {
-            _scannedValues.add(productData['Barcode']);
+          if (!_scannedProducts
+              .any((product) => product['Barcode'] == productData['Barcode'])) {
+            _scannedProducts.add(productData);
           }
         });
       } else {
-        // Handle case where no product is found
         print('Product not found');
         showModalBottomSheet(
           context: context,
           isScrollControlled: true,
-          constraints: BoxConstraints(maxHeight: 700, minHeight: 600),
+          constraints: const BoxConstraints(maxHeight: 700, minHeight: 600),
           builder: (context) => NabhetekoProductPage(),
         );
-        // Navigator.push(context, MaterialPageRoute(builder: (context) {
-        //   return NabhetekoProductPage();
-        // }));
       }
     } catch (e) {
       print('Error searching barcode: $e');
@@ -64,18 +59,74 @@ class _NewbillState extends State<Newbill> {
 
   void _handleDelete(int index) {
     setState(() {
-      _scannedValues.removeAt(index);
+      _scannedProducts.removeAt(index);
     });
   }
 
   void handleScanResult(BarcodeCapture capture) {
-    setState(() {
-      for (final barcode in capture.barcodes) {
-        final String? code = barcode.rawValue;
-        if (code != null && !_scannedValues.contains(code)) {
-          _scannedValues.add(code);
-        }
+    for (final barcode in capture.barcodes) {
+      final String? code = barcode.rawValue;
+      if (code != null) {
+        _searchBarcode(code);
       }
+    }
+  }
+
+  void _editProduct(int index) {
+    final product = _scannedProducts[index];
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditProductPage(
+          initialProductName: product['ProductName'] ?? '',
+          initialMRP: (product['MRP'] ?? 0.0).toDouble(),
+          initialPrice: (product['Price'] ?? 0.0).toDouble(),
+          initialWholesalePrice: (product['WholesalePrice'] ?? 0.0).toDouble(),
+          initialQuantity: (product['Quantity'] ?? 0.0).toDouble(),
+          initialDiscount: (product['Discount'] ?? 0.0).toDouble(),
+        ),
+      ),
+    ).then((editedProduct) {
+      if (editedProduct != null) {
+        setState(() {
+          _scannedProducts[index] = editedProduct;
+        });
+      }
+    });
+  }
+
+  void _increaseQuantity(int index) {
+    setState(() {
+      _scannedProducts[index]['Quantity'] += 1;
+    });
+  }
+
+  void _decreaseQuantity(int index) {
+    setState(() {
+      if (_scannedProducts[index]['Quantity'] > 0) {
+        _scannedProducts[index]['Quantity'] -= 1;
+      }
+    });
+  }
+
+  int getTotalQuantity() {
+    return _scannedProducts.fold(0, (sum, product) {
+      final int quantity = product['Quantity'] is int
+          ? product['Quantity'] as int
+          : (product['Quantity'] as double).toInt();
+      return sum + quantity;
+    });
+  }
+
+  double getTotalPrice() {
+    return _scannedProducts.fold(0.0, (sum, product) {
+      final double price = product['Price'] is int
+          ? (product['Price'] as int).toDouble()
+          : product['Price'];
+      final int quantity = product['Quantity'] is int
+          ? product['Quantity']
+          : (product['Quantity'] as double).toInt();
+      return sum + (price * quantity);
     });
   }
 
@@ -83,7 +134,7 @@ class _NewbillState extends State<Newbill> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 123, 139, 123),
+        backgroundColor: const Color.fromARGB(255, 123, 139, 123),
         title: const Text("Naya Bill"),
       ),
       body: Stack(
@@ -108,7 +159,7 @@ class _NewbillState extends State<Newbill> {
                       "Scan Barcode",
                       style:
                           TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -124,15 +175,78 @@ class _NewbillState extends State<Newbill> {
                   ),
                   onSubmitted: (value) {
                     _searchBarcode(value);
-                    barcodeController
-                        .clear(); // Clear the text field after submission
+                    barcodeController.clear();
                   },
                 ),
               ),
               Expanded(
-                child: GrossaryListStaff(
-                  onDelete: _handleDelete,
-                  scannedValues: _scannedValues,
+                child: ListView.builder(
+                  itemCount: _scannedProducts.length,
+                  itemBuilder: (context, index) {
+                    final product = _scannedProducts[index];
+                    return ListTile(
+                      leading: product['ProductImage'] != null
+                          ? Image.network(product['ProductImage'], width: 50)
+                          : const Icon(Icons.shopping_bag),
+                      title: Text(product['ProductName'] ?? 'Unknown Product'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Price: ${product['Price']?.toString() ?? 'N/A'}'),
+                          Text(
+                              'Quantity: ${product['Quantity']?.toString() ?? '0'}'),
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.remove),
+                            onPressed: () => _decreaseQuantity(index),
+                          ),
+                          Text(
+                            product['Quantity']?.toString() ?? '0',
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.add),
+                            onPressed: () => _increaseQuantity(index),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () => _editProduct(index),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => _handleDelete(index),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total Quantity: ${getTotalQuantity()}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Total Price: Rs. ${getTotalPrice().toStringAsFixed(2)}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle checkout logic here
+                      },
+                      child: const Text('Checkout'),
+                    ),
+                  ],
                 ),
               ),
             ],
